b646ac92ea6c34412511d95f92d078f7e98a0eb3
cesar <cesarcachulo95@live.com.pt>
2023-01-13 19:54:23 +0000

    Developments

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-input');
 const mealList = document.getElementById('meal-list');
 const mealDetails = document.querySelector('meal-details-content');
 const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
 searchBtn.addEventListener('click', getMealList);
+searchInput.addEventListener('keyup', function() {
+    if (event.key === 'Enter') {
+        getMealList();
+    }
+});
+mealList.addEventListener('click', getMealRecipe);
 
 function getMealList() {
     let searchInputTxt = document.getElementById('search-input').value.trim();
@@ -21,7 +28,7 @@ function getMealList() {
                         </div>
                         <div class="meal-name">
                             <h3>${meal.strMeal}</h3>
-                            <a href="#" class="recipe-btn">Get recipe</a>
+                            <a href="#" class="recipe-btn">See Recipe</a>
                         </div>
                     </div>
                 `;
@@ -33,4 +40,51 @@ function getMealList() {
         }
         mealList.innerHTML = html;
     })
+}
+
+function getMealRecipe(e){
+    console.log('asdas')
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealId = e.target.parentElement.parentElement.getAttribute('data-id');
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+            .then(response => response.json())
+            .then(data => showRecipeModal(data.meals[0]));
+    }
+}
+
+function showRecipeModal(meal) {
+    const html = `
+        <div class="recipe-modal" id="recipe-modal">
+            <button type="button" class="btn recipe-close-btn" id="recipe-close-btn">
+                <i class="fas fa-times"></i>
+            </button>
+
+            <div class="recipe-content">
+                <h2 class="recipe-title">${meal.strMeal}</h2>
+                <p class="recipe-category">${meal.strCategory}</p>
+                <div class="recipe-instruct">
+                    <h3>Instructions:</h3>
+                    <p>${meal.strInstructions}</p>
+                </div>
+                <div class="recipe-meal-img">
+                    <img src="${meal.strMealThumb}" alt="">
+                </div>
+                <div class="recipe-link">
+                    <a href="${meal.strYoutube}" target="_blank">Watch Video</a>
+                </div>
+            </div>
+        </div>
+    `;
+    document.getElementById('recipe-modal-container').innerHTML = html;
+    document.addEventListener('click', handleRecipeModalOutsideClick);
+}
+
+function handleRecipeModalOutsideClick(e) {
+    const recipeModal = document.getElementById('recipe-modal');
+    const isClickOutsideModal = !recipeModal.contains(e.target);
+    if (isClickOutsideModal) {
+        document.removeEventListener('click', handleRecipeModalOutsideClick);
+        recipeModal.remove();
+    }
 }
